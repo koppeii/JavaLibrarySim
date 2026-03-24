bb9761916db7b6b00d9cf115618a3716623bc217
Alex <cryptoyram@gmail.com>
2026-03-24 11:28:49 -0500

    initial commit

---
+++ src/Library.java
@@ -0,0 +1,13 @@
+//Represents the entire library system
+class Library {
+
+    int currentDay; // Current simulation day
+    int currentHour;
+
+    // add more fields here
+
+
+    // add constructor(s) here
+
+
+}
