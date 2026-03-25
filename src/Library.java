478eee664d8903a5f52f9266d6b53bb759d1bb70
unknown <alturaram@gmail.com>
2026-03-24 19:53:35 -0500

    whole buncha dilly dally, line 26 error, will figure out some other time.

--- src/Library.java
+++ src/Library.java
@@ -1,12 +1,36 @@
 //Represents the entire library system
+
+import java.util.ArrayList;
 class Library {
 
     int currentDay; // Current simulation day
     int currentHour;
+    ArrayList<String> books = new ArrayList<>(10);
+    ArrayList<String> removedBooks = new ArrayList<>(10);
+    ArrayList<String> members = new ArrayList<>(10);
 
     // add more fields here
+    public void Books() {
+        books.add("Farenheit 451");
+        books.add("The Bible");
+    }
 
-    int x = 1;
+    public void returnBook(int x) {
+        String book = removedBooks.get(x);
+        books.add(x, book);
+        removedBooks.remove(book);
+    }
+    public void removeBooks(int x) {
+        String book = books.get(x);
+        removedBooks.add(book);
+        books.remove(book);
+    }
+    public void Members() {
+        members.add("John");
+        members.add("Micheal");
+    }
+
+
     // add constructor(s) here
 
 
--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,37 @@ public class Main {
     public static void main(String[] args) {
 
         Library lib = new Library();
-
-        int simulationLength = 10;
+        lib.Books();
+        lib.Members();
+        int simulationLength = 100;
 
         while (lib.currentDay < simulationLength) {
-            
+
             // Advance time
             lib.currentDay++;
             System.out.println("\n--- Day " + lib.currentDay + " ---");
 
+            System.out.println(lib.books);
+
             // simulate a random event
+
             int randomNumber = Rand.randomInt(0, 4); // could generate 0, 1, 2, or 3
+            switch (randomNumber) {
+                case 1:
+                    lib.removeBooks(1);
+
+                case 2:
+                    if (lib.books.get(1).isEmpty()) {
+                    lib.returnBook(0);
+                }
+                    else {
+                        System.out.println(121313);
+                    }
+
+
+//                randomNumber == 2 && lib.books.get(1).isEmpty()) {
+//                    lib.returnBook(1);
+            }
 
             switch (randomNumber) {
 
