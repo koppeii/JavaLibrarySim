a47758dea71a7c2801fb97614f494fd2b419666b
NPSK <cryptoyram@gmail.com>
2026-03-25 15:02:53 -0500

    want to add weights to the actions to be more a more realistic simulation

--- src/Main.java
+++ src/Main.java
@@ -24,33 +24,41 @@ public class Main {
             // simulate a random event
 
             for (int i = 0; i < simulationFrequency; i++) {
-                int randomNumber = Rand.randomInt(0, 6);
+                // should add weights to simulate more realistic actions
 
-                switch (randomNumber) {
-                    // reformat, the odds of someone entering the library, and applying for a membership, and taking out a book is 1/216
-                    case 1:
-                        library.enter(helper.randomIndividual(library.members));
-                    case 2:
-                        library.leave(helper.randomPersonInLibrary(library.presentInLibrary));
-                    case 3:
-                        library.applyMembership(helper.randomPersonInLibrary(library.presentInLibrary));
-                    case 4:
-                        library.revokeMembership(helper.randomMember(library.members));
-                    case 5:
-                        library.loanRandomBook(helper.randomMember(library.members), helper.randomBook(library.books));
-                    case 6:
-                        Member randomMember = helper.randomMember(library.members);
+                int inOut = Rand.randomInt(0, 6);
 
-                        if (randomMember != null) {
-                            library.returnRandomBook(randomMember, helper.randomBook(randomMember.loanedBooks.books));
-                        };
+                if (inOut == 0)
+                    library.enter(helper.randomIndividual(library.members));
+                else if (inOut == 1)
+                    library.leave(helper.randomPersonInLibrary(library.presentInLibrary));
+
+                for (int p = 0; p < library.presentInLibrary.size(); p++) {
+                    // why isnt this being called?
+                    int randomAction = Rand.randomInt(0, 4);
+
+//                    System.out.println(randomAction);
+
+                    switch (randomAction) {
+                        case 0:
+                            library.applyMembership(library.presentInLibrary.get(p));
+                        case 1:
+                            library.revokeMembership(helper.randomMember(library.members));
+                        case 2:
+                            library.loanRandomBook(helper.randomMember(library.members), helper.randomBook(library.books));
+                        case 3:
+                            Member randomMember = helper.randomMember(library.members);
+
+                            if (randomMember != null) {
+                                library.returnRandomBook(randomMember, helper.randomBook(randomMember.loanedBooks.books));
+                            };
+                        }
+                    }
                 }
-            }
-
             if (library.currentDay >= simulationLength)
                 Input.waitForUserToPressEnter("Press Enter to end the simulation.");
             else
                 Input.waitForUserToPressEnter("Press Enter to simulate the next day.");
-        }
+            }
     }
 }
